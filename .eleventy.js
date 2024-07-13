0571bc096b8629feaf60941ec55fd4e2b7cbbb1b
CosmicKittyUwU <CosmicKittyUwU@users.noreply.github.com>
2024-07-13 22:55:43 +0000

    deploy: f8bf2158df2ddbf88272ce4b79e263eaba74fdfb

--- .eleventy.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (eleventyConfig) {
-	// Input directory: src
-	// Output directory: _site
-
-	// The following copies to `_site/img`
-	eleventyConfig.addPassthroughCopy("gifs");
-};
